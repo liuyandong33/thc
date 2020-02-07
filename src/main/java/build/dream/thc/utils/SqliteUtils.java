e7808f1e4bc3dc1ad0b68007dbcd9efddbd15ac9
liuyandong <554030404@qq.com>
2020-02-07 17:55:17 +0800

    初始化项目！！

--- src/main/java/build/dream/thc/utils/SqliteUtils.java
+++ src/main/java/build/dream/thc/utils/SqliteUtils.java
@@ -5,6 +5,8 @@ import build.dream.thc.domains.Token;
 import java.sql.*;
 
 public class SqliteUtils {
+    private static final String THC_DB_URL = "jdbc:sqlite://e:/thc.db";
+
     static {
         try {
             Class.forName("org.sqlite.JDBC");
@@ -17,7 +19,7 @@ public class SqliteUtils {
         Connection connection = null;
         PreparedStatement preparedStatement = null;
         try {
-            connection = DriverManager.getConnection("jdbc:sqlite://e:/thc.db");
+            connection = DriverManager.getConnection(THC_DB_URL);
             preparedStatement = connection.prepareStatement("INSERT INTO token(access_token, token_type, expires_in, scope, fetch_time) VALUES (?, ?, ?, ?, ?)");
             preparedStatement.setString(1, token.getAccessToken());
             preparedStatement.setString(2, token.getTokenType());
@@ -26,9 +28,10 @@ public class SqliteUtils {
             preparedStatement.setDate(5, token.getFetchTime());
             return preparedStatement.executeUpdate();
         } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
             DatabaseUtils.closeStatement(preparedStatement);
             DatabaseUtils.closeConnection(connection);
-            throw new RuntimeException(e);
         }
     }
 
@@ -36,11 +39,11 @@ public class SqliteUtils {
         Connection connection = null;
         Statement statement = null;
         ResultSet resultSet = null;
-        Token token = null;
         try {
-            connection = DriverManager.getConnection("jdbc:sqlite://e:/thc.db");
+            connection = DriverManager.getConnection(THC_DB_URL);
             statement = connection.createStatement();
             resultSet = statement.executeQuery("SELECT * from token");
+            Token token = null;
             if (resultSet.next()) {
                 token.setAccessToken(resultSet.getString("access_token"));
                 token.setTokenType(resultSet.getString("token_type"));
@@ -48,21 +51,21 @@ public class SqliteUtils {
                 token.setScope(resultSet.getString("scope"));
                 token.setFetchTime(resultSet.getDate("fetch_time"));
             }
+            return token;
         } catch (Exception e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         } finally {
             DatabaseUtils.closeResultSet(resultSet);
             DatabaseUtils.closeStatement(statement);
             DatabaseUtils.closeConnection(connection);
         }
-        return token;
     }
 
     public static int deleteToken() {
         Connection connection = null;
         Statement statement = null;
         try {
-            connection = DriverManager.getConnection("jdbc:sqlite://e:/thc.db");
+            connection = DriverManager.getConnection(THC_DB_URL);
             statement = connection.createStatement();
             return statement.executeUpdate("DELETE FROM token");
         } catch (Exception e) {
